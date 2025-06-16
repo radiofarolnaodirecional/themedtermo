67763fb5d691011d69052bef7bb38141fa5f8815
radiofarolnaodirecional <143708402+radiofarolnaodirecional@users.noreply.github.com>
2025-06-16 02:31:10 -0300

    whitenoise

--- server/main.py
+++ server/main.py
@@ -1,12 +1,15 @@
 from flask import Flask;
 from views import view;
 from api import api;
+from whitenoise import WhiteNoise;
 import os;
 
 app = Flask(__name__,
             template_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'ui', 'templates'),
             static_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'ui', 'static')
         ); 
+
+app.wsgi_app = WhiteNoise(app.wsgi_app, root=app.static_folder); 
 
 app.register_blueprint(view); 
 app.register_blueprint(api); 
